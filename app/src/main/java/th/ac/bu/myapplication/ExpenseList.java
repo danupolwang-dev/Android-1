d2b9a4f6308f1cfc6fcd46378acefd23f74f49a3
DanupolWang <danupol.wang@bumail.net>
2025-04-04 02:45:47 +0700

    add menu bar

---
+++ app/src/main/java/th/ac/bu/myapplication/ExpenseList.java
@@ -0,0 +1,15 @@
+package th.ac.bu.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+public class ExpenseList extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_expense_list, container, false);
+    }
+}
